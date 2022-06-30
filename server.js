bdc7f197fe3515dc052b3b09075c2083e852396a
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-06-30 18:39:41 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const { request } = require('express')
+const express = require('express')
+const app = express()
+const PORT = 2000
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server runnning on port ${PORT}`)
+})
