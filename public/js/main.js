c1a763edcb4cd0d56bbe2d85c5e5559846400885
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-07-02 01:55:46 -0400

    updated post method

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,21 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-//initialize a starting position for the drinks array
-let i = 0;
+const { features } = require("process");
 
 //set an event listener for the button that when clicked, triggers the function getDrink
-document.getElementById('inputSubmitQuery').addEventListener('click', getDrink)
+document.getElementById('inputSubmitQuery').addEventListener('submit', getDrink)
 
 //initialize and define function getDrink
 function getDrink(){
-
+    console.log('hi')
     //gather the user input of which drink they would like to see by using queryselector value
     let drink = document.getElementById('inputText').value;
 
-    //using template literals to plug in the user selected drink into the API search
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
-
-    
-    //api template
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            //fill in html elements with the title of the drink, the image of the drink, and the instructions for the drink
-            console.log(data)
-        })
-        //error catch and display error
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
+    fetch('/submitDrink',{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'text/plain'
+        },
+        body: drink
+    })
 }
--- server.js
+++ server.js
@@ -4,14 +4,30 @@ const app = express()
 const PORT = 2000
 
 app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname+'/views/')
 app.use(express.json())
 
 app.get('/', (request, response) =>{
-    response.sendFile(__dirname + '/index.html')
+    response.render('/index.html')
 })
 
 app.post('/submitDrink',(request,response) =>{
     
+    let drink = request.body
+    console.log(drink)
+
+    // const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
+    // //api template
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         //fill in html elements with the title of the drink, the image of the drink, and the instructions for the drink
+    //         console.log(data)
+    //     })
+    //     //error catch and display error
+    //     .catch(err => {
+    //         console.log(`error ${err}`)
+    //     });
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
