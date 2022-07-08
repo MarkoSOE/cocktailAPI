23214e9462b5dd3e07a16e2c5040286e68742850
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-07-08 01:00:11 -0400

    combined measure and ingredients

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.get('/', (request, response) =>{
 
 app.post('/submitDrink',(request,response) =>{
     
-    console.log('req.name: ', request.name);
-    console.log('req.data: ', request.data);
     console.log('req.body: ', request.body.textinput);
 
     let drink = request.body.textinput
@@ -44,10 +42,36 @@ app.post('/submitDrink',(request,response) =>{
             //drink glass
             let drinkGlass = data.drinks[i].strGlass
             console.log(drinkGlass)
-            //instructions
-
-            
             //ingredients
+            //create an array to store the ingredients
+            let ingredients = []
+            ingredients.push(data.drinks[i].strIngredient1, data.drinks[i].strIngredient2, data.drinks[i].strIngredient3, data.drinks[i].strIngredient4, data.drinks[i].strIngredient5,data.drinks[i].strIngredient6,data.drinks[i].strIngredient7,data.drinks[i].strIngredient8,data.drinks[i].strIngredient9,data.drinks[i].strIngredient10,data.drinks[i].strIngredient11,data.drinks[i].strIngredient12,data.drinks[i].strIngredient13,data.drinks[i].strIngredient14,data.drinks[i].strIngredient15)
+
+            //measure
+            //create an array to store the measures
+            let measures = []
+            measures.push(data.drinks [i].strMeasure1 , data.drinks [i].strMeasure2, data.drinks [i].strMeasure3, data.drinks [i].strMeasure4, data.drinks [i].strMeasure5, data.drinks [i].strMeasure6, data.drinks [i].strMeasure7, data.drinks [i].strMeasure8, data.drinks [i].strMeasure9, data.drinks [i].strMeasure10, data.drinks [i].strMeasure11, data.drinks [i].strMeasure12, data.drinks [i].strMeasure13, data.drinks [i].strMeasure14, data.drinks [i].strMeasure15)
+
+            ingredients = ingredients.filter(function(e){
+                return e
+            })
+
+			measures = measures.filter(function(e){
+                return e
+            })
+
+            console.log(ingredients)
+            console.log(measures)
+
+            let combinedingredientsinstructions = []
+            
+            for(let i = 0; i < ingredients.length; i++)
+
+            //instructions
+            let instructions = data.drinks[i].strInstructions
+
+			console.log(instructions)
+
         })
         //error catch and display error
         .catch(err => {
