7124649d83b98d9ba452e710bc1b6a66432aabfa
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-07-11 21:49:23 -0400

    latest working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 const PORT = 2000
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
 
 app.set('views', __dirname+'/views/')
 app.set('view engine', 'ejs')
